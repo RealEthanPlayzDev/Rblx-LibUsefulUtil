e50e76d80c77e36cf7559430fee3773f01e4e771
EthanPlayz <ethanbudiantodev@gmail.com>
2021-07-16 20:54:38 +0700

    Fix call meta and added a newindex and metatable meta

--- LibUsefulUtility/init.lua
+++ LibUsefulUtility/init.lua
@@ -5,11 +5,13 @@ Author: RadiatedExodus (RealEthanPlayz)
 
 local ci = script:GetChildren()
 return setmetatable(ci, {
-	__call = function(t, utilname)
+	__newindex = function() end,
+	__call = function(utilname)
 		local indexed = ci[table.find(ci, utilname or nil)]
 		if indexed then
 			return require(indexed)
 		end
 		return false
 	end,
+	__metatable = "Locked"
 })
