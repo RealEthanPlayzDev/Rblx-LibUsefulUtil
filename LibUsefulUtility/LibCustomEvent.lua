300241b9813b0ac89df886a263c5b964b8884f13
EthanPlayz <ethanbudiantodev@gmail.com>
2021-07-16 22:52:30 +0700

    Finish LibCustomEvent

--- LibUsefulUtility/LibCustomEvent.lua
+++ LibUsefulUtility/LibCustomEvent.lua
@@ -5,29 +5,38 @@ Author: RadiatedExodus (RealEthanPlayz)
 
 local LibCustomEvent = {}
 LibCustomEvent.__index = LibCustomEvent
-LibCustomEvent.__newindex = function() end
+--// LibCustomEvent.__newindex = function() end
 LibCustomEvent.__metatable = "The metatable is locked"
 
-function LibCustomEvent.new()
-    local self = setmetatable({}, LibCustomEvent)
-
-    self.events = {}
-    self.isDestroyed = false
-
-    return self
-end
-
 function LibCustomEvent:Fire(...)
+    assert(not self.isDestroyed, [[LibCustomEvent: this event has been destroyed and no longer can be used]])
     for _, v in ipairs(self.events) do
+        if not typeof(v) == "function" then continue end
         v(...)
     end
     return
 end
 
 function LibCustomEvent:Connect(func)
+    assert(not self.isDestroyed, [[LibCustomEvent: this event has been destroyed and no longer can be used]])
     assert(typeof(func) == "function", [[LibCustomEvent: invalid argument #1 to 'Connect' (function expected, got ]]..typeof(func)..[[)]])
     table.insert(self.events, #self.events + 1, func)
     return
 end
 
-return LibCustomEvent
+function LibCustomEvent:Destroy()
+    self.isDestroyed = true
+    table.clear(self.events)
+    return
+end
+
+return {
+	new = function()
+		local newevent = setmetatable({}, LibCustomEvent)
+
+		newevent.events = {}
+		newevent.isDestroyed = false
+
+		return newevent
+	end
+}
